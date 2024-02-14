0837a488c9633464625da55558333bde8f824a7a
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-14 14:54:40 +0000

    Initialized deliveries table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_14_144358) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_14_145239) do
+  create_table "deliveries", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "description"
+    t.string "details"
+    t.date "supposed_to_arrive_on"
+    t.boolean "arrived"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
